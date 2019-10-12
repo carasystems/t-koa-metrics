164a2f43e5c367a944b583808fe0f1e4776a0fed
d2bot <d2botroot@gmail.com>
2019-10-12 22:11:42 +0800

    feature: switch to cls to hold global vars

--- test/tracer-test.js
+++ test/tracer-test.js
@@ -2,6 +2,7 @@
 const request = require('supertest');
 const Koa = require('koa');
 const { expect } = require('chai');
+const uuid = require('uuid');
 
 const koaMetrics = require('../lib')({
   app: 'my-sample-app',
@@ -81,6 +82,58 @@ describe('superagent tracer tests', () => {
             .equal(expectedTracerId);
           done();
         }
+      });
+  });
+
+  it('should return correct traceId for concurrent reqeust', () => {
+    const requestId1 = uuid.v4();
+    const requestId2 = uuid.v4();
+
+    const app = Koa();
+
+    app.use(koaMetrics.tracer);
+    app.use(route.get('/request', function* handler() {
+      expect(this.superagent)
+        .to
+        .be
+        .an
+        .instanceOf(Object);
+      expect(this.superagent)
+        .to
+        .have
+        .property('get');
+      const res = yield this.superagent.get('http://127.0.0.1:9999/request2');
+      this.body = res.text;
+      return this.body;
+    }));
+
+    const app2 = Koa();
+    app2.use(koaMetrics.tracer);
+    // eslint-disable-next-line require-yield
+    app2.use(route.get('/request2', function* handler() {
+      this.body = this.tracerInfo.tracerId;
+      return this.body;
+    }));
+    app2.listen(9999);
+
+    const testServer = request(app.listen());
+    return Promise.all([
+      testServer.get('/request')
+        .set('x-thimble-tracer-id', requestId1)
+        .expect(200)
+        .then(res => res.text),
+      testServer.get('/request')
+        .set('x-thimble-tracer-id', requestId2)
+        .expect(200)
+        .then(res => res.text),
+    ])
+      .then(([id1, id2]) => {
+        expect(id1)
+          .to
+          .equal(requestId1);
+        expect(id2)
+          .to
+          .equal(requestId2);
       });
   });
 });
